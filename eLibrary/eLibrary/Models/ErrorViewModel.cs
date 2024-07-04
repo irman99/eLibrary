8d4d68e9530e584154f4c724ad830fc2c44678bf
irman99 <irman.isic@edu.fit.ba>
2024-07-04 03:28:29 +0200

    Postavljanje

---
+++ eLibrary/eLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
